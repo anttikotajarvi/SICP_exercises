f5f2061add9f76e894d2cbbe470a110377c50330
Antti Kotajarvi <jaypacket@gmail.com>
2026-04-27 23:14:45 +0300

    sets

--- exercises/2/59.rkt
+++ exercises/2/59.rkt
@@ -1,7 +1,22 @@
-; SICP 2.3.3
+;; SICP 2.3.3
+; Chapter: Building Abstractions with Data
+; Section: Symbolic Data
+; Subsection: Example: Representing Sets
+; Exercise 2.59: SICP 2.3.3
 ; Chapter: Building Abstractions with Data
 ; Section: Symbolic Data
 ; Subsection: Example: Representing Sets
 ; Exercise 2.59:
+; Implement the 'union-set' operation for the unordered-list
+;  representation of sets.
 ; ______________________________________________________________
 #lang sicp
+(#%require "../lib.rkt")
+
+(define (union-set set1 set2)
+  (if (null? set1)
+      set2
+      (union-set (cdr set1)
+                 (adjoin-set (car set1) set2))))
+
+(inspect (union-set '(1 2 3) '(3 4 5))) ; (2 1 3 4 5)
--- exercises/lib.rkt
+++ exercises/lib.rkt
@@ -328,3 +328,21 @@
 (define (base s) (cadr s))
 (define (exponent s) (caddr s))
 (#%provide make-expt expt? base exponent)
+
+;; 2.3.3. Sets
+(define (element-of-set? x set)
+  (cond [(null? set) false]
+        [(equal? x (car set)) true]
+        [else (element-of-set? x (cdr set))]))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond [(or (null? set1) (null? set2)) '()]
+        [(element-of-set? (car set1) set2)]
+        [(cons (car set1) (intersection-set (cdr set1) set2))]
+        [else (intersection-set (cdr set1) set2)]))
+(#%provide element-of-set? adjoin-set intersection-set)
