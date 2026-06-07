48faaba9cfc078f064d533923984afa5e30f030a
Antti Kotajarvi <jaypacket@gmail.com>
2026-06-07 19:59:40 +0300

    dense term implementation

--- exercises/2/87.rkt
+++ exercises/2/87.rkt
@@ -2,7 +2,7 @@
 ; Chapter: Building Abstractions with Data
 ; Section: Systems with Generic Operations
 ; Subsection: Example: Symbolic Algebra
-; Exercise 2.83: 
+; Exercise 2.87: 
 ; Install '=zero?' for polynomials in the generic arithmetic 
 ;  package.  This will allow 'adjoin-zero' to work for 
 ;  polynomials with coefficients that are themselves polynomials.
--- exercises/2/88.rkt
+++ exercises/2/88.rkt
@@ -2,7 +2,7 @@
 ; Chapter: Building Abstractions with Data
 ; Section: Systems with Generic Operations
 ; Subsection: Example: Symbolic Algebra
-; Exercise 2.83: 
+; Exercise 2.88: 
 ; Extend the polynomial system to include subtraction of 
 ;  polynomials. (Hint: You may find it helpful to define a 
 ;  generic negation operation.)
